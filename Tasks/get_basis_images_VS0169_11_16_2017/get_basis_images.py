6018ef67a2a8333f39ab8c11208d7d5e5db239f0
Scott Trinkle <tscott.trinkle@gmail.com>
2017-11-17 16:03:29 -0600

    Updated quest for numerical density projections...

--- Tasks/get_basis_images_VS0169_11_16_2017/get_basis_images.py
+++ Tasks/get_basis_images_VS0169_11_16_2017/get_basis_images.py
@@ -1,47 +1,79 @@
+'''
+This script is attempting to construct U, Os and H2O basis images from the
+L-edge bracketed VSO169 images
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 from libtiff import TIFF
 from scipy.interpolate import interp1d
+from microct.phasefunctions import read_data
 
-# Uranium
-# Nominal density: 1.892e1 g/cm3
-# E keV             u/p  cm2/g
+# E in keV, u/p in cm^2 / g
+H2O_E, H2O_u = read_data('../../Data/NIST/u_p_H2O.txt')
+U_E, U_u = read_data('../../Data/NIST/u_p_U.txt')
+Os_E, Os_u = read_data('../../Data/NIST/u_p_Os.txt')
 
-U = np.array([[1.708047E+01,  4.4787E+01],
-              [1.714913E+01,  4.4345E+01],
-              [1.725213E+01,  1.0327E+02],
-              [1.750963E+01,  9.9419E+01],
-              [1.759961E+01,  9.8122E+01]])
+U = interp1d(U_E, U_u, kind='linear')
+Os = interp1d(Os_E, Os_u, kind='linear')
+H2O = interp1d(H2O_E, H2O_u, kind='linear')
 
-# Osmium
-# Nominal density: 2.253e1 g/cm3
-# E kev           u/p cm2/g
-Os = np.array([[1.065348E+01,  8.5388E+01],
-               [1.081655E+01, 8.2028E+01],
-               [1.086003E+01,  8.1166E+01],
-               [1.092525E+01,  2.0788E+02],
-               [1.103212E+01,  2.0250E+02]])
+EOsL = 10.82
+EOsH = 10.92
+EUL = 17.12
+EUH = 17.22
 
-# Water
-H2O = np.array([[7.902609E+00,  1.027736E+01],
-                [8.447890E+00,  8.464951E+00],
-                [9.030794E+00,  6.971115E+00],
-                [9.653919E+00,  5.721777E+00],
-                [1.032004E+01,  4.696412E+00],
-                [1.103212E+01,  3.855846E+00],
-                [1.179334E+01,  3.174232E+00],
-                [1.260708E+01,  2.621632E+00]])
 
-U_interp_func = interp1d(U[:, 0], U[:, 1], kind='linear')
-Os_interp_func = interp1d(Os[:, 0], Os[:, 1], kind='linear')
-H2O_interp_func = interp1d(H2O[:, 0], H2O[:, 1], kind='linear')
+def du(u_func, EL, EH):
+    return u_func(EH) - u_func(EL)
 
-U_1712 = U_interp_func(17.12)
-U_1722 = U_interp_func(17.22)
-H2O_1712 = H2O_interp_func(17.12)
-H2O_1722 = H2O_interp_func(17.22)
 
-Os_1082 = Os_interp_func(10.82)
-Os_1092 = Os_interp_func(10.92)
-H2O_1082 = H2O_interp_func(10.82)
-H2O_1092 = H2O_interp_func(10.92)
+G_1 = TIFF.open(
+    '../L_edge_raw_subtraction_VS0169_11_15_2017/new_subs/tifs/Os_low_sino_225.tif', 'r').read_image()
+G_2 = TIFF.open(
+    '../L_edge_raw_subtraction_VS0169_11_15_2017/new_subs/tifs/Os_high_sino_225.tif', 'r').read_image()
+G_3 = TIFF.open(
+    '../L_edge_raw_subtraction_VS0169_11_15_2017/new_subs/tifs/U_low_sino_225.tif', 'r').read_image()
+G_4 = TIFF.open(
+    '../L_edge_raw_subtraction_VS0169_11_15_2017/new_subs/tifs/U_high_sino_225.tif', 'r').read_image()
+
+
+'''
+Four methods:
+(1) metal and "other"
+(2) 2x2 metal and water
+(3) 2x2 metal and metal
+(4) 4x3 all three
+'''
+
+# Metal and "other"
+
+
+def other(g_H, g_L, du):
+    return (g_H - g_L) / du
+
+
+aU_o = other(G_3, G_4, du(U, EUL, EUH))
+aOs_o = other(G_2, G_1, du(Os, EOsL, EOsH))
+
+
+# Metal and "water"
+
+
+def twobytwo(imL, imH, u1, u2, EH, EL):
+    U_mat_inv = np.linalg.inv(np.array([[u1(EH), u2(EH)],
+                                        [u1(EL), u2(EL)]]))
+    rows, cols = imL.shape
+    A = np.zeros((2, rows, cols))
+    for row in np.arange(rows):
+        for col in np.arange(cols):
+            g = np.array([imH[row, col], imL[row, col]])
+            a = np.matmul(U_mat_inv, g)
+            A[:, row, col] = a
+    return A[0]
+
+
+print('Calculating Os and water...')
+aOs_wat = twobytwo(G_1, G_2, Os, H2O, EOsL, EOsH)
+print('Calculating U and water...')
+aU_wat = twobytwo(G_3, G_4, U, H2O, EUL, EUH)
